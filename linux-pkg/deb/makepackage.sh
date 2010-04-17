f0433445d56e2fdd1abb11fd4272f5b9f89b1cc2
jwolz <jwolz@localhost>
2010-04-17 14:54:08 +0200

    Version 0.4.3

--- linux-pkg/deb/makepackage.sh
+++ linux-pkg/deb/makepackage.sh
@@ -37,9 +37,16 @@ cp -a template $TARGETDIR
 pushd $WORKSPACE
 cp build/yajhfc.jar $TARGETDIR
 cp doc/faq.html doc/faq.css doc/footnote.png doc/faq.pdf README.txt $TARGETDIR/doc
+cd ../FOPPlugin
+cp build/FOPPlugin.jar $TARGETDIR/fopplugin
+cp dist/readme.pdf $TARGETDIR/fopplugin
+cp -R dist/examples $TARGETDIR/fopplugin/examples
 popd
 
-cat - template/debian/changelog <<EOF >$TARGETDIR/debian/changelog
+if grep -c "yajhfc ($PACKAGEVERSION) unstable" template/debian/changelog > /dev/null; then
+  true # Do nothing 
+else
+  cat - template/debian/changelog <<EOF >$TARGETDIR/debian/changelog
 yajhfc ($PACKAGEVERSION) unstable; urgency=low
 
   * Package build for YajHFC $VERSION
@@ -48,7 +55,8 @@ yajhfc ($PACKAGEVERSION) unstable; urgency=low
 
 EOF
 
-cp $TARGETDIR/debian/changelog template/debian/changelog.full
+  cp $TARGETDIR/debian/changelog template/debian/changelog.full
+fi;
 
 echo 'Building Debian packets...'
 pushd $TARGETDIR
@@ -57,13 +65,21 @@ dpkg-buildpackage -rfakeroot
 cd ..
 cp *.deb $DEBDIR
 
+
 echo 'Updating repository...'
-cp *.deb $REPOSITORY/stable
+cp *.deb    $REPOSITORY/stable/deb-all
+cp *.dsc    $REPOSITORY/stable/sources
+cp *.tar.gz $REPOSITORY/stable/sources
 
 cd $REPOSITORY
 apt-ftparchive packages stable > stable/Packages
 gzip -c9 stable/Packages > stable/Packages.gz
 bzip2 -c stable/Packages > stable/Packages.bz2
+
+apt-ftparchive sources stable > stable/Sources
+gzip -c9 stable/Sources > stable/Sources.gz
+bzip2 -c stable/Sources > stable/Sources.bz2
+
 
 apt-ftparchive -o 'APT::FTPArchive::Release::Suite=stable' release stable > stable/Release
 gpg -u jonas.wolz@freenet.de --output stable/Release.gpg -ba stable/Release
