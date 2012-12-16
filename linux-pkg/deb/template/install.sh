efbf2603fcf910ac8c5f9c97b467d65fb6089b93
Jonas Wolz <jonas@yajhfc.de>
2012-12-16 12:13:25 +0100

    Updated linux packages

--- linux-pkg/deb/template/install.sh
+++ linux-pkg/deb/template/install.sh
@@ -28,6 +28,18 @@ mkdir -p $PREFIX/share/pixmaps
 install -m 0644 yajhfc-48x48.png $PREFIX/share/pixmaps/yajhfc.png
 install -m 0644 yajhfc.xpm $PREFIX/share/pixmaps/yajhfc.xpm
 
+mkdir -p $PREFIX/share/icons/hicolor/128x128/apps
+install -m 0644 logo-large.png $PREFIX/share/icons/hicolor/128x128/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/16x16/apps
+install -m 0644 yajhfc-16x16.png $PREFIX/share/icons/hicolor/16x16/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/32x32/apps
+install -m 0644 yajhfc-32x32.png $PREFIX/share/icons/hicolor/32x32/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/48x48/apps
+install -m 0644 yajhfc-48x48.png $PREFIX/share/icons/hicolor/48x48/apps/yajhfc.png
+
 mkdir -p $PREFIX/share/applications
 install -m 0644 yajhfc.desktop $PREFIX/share/applications
 
--- linux-pkg/rpm/template/install.sh
+++ linux-pkg/rpm/template/install.sh
@@ -21,10 +21,24 @@ install -m 0644 yajhfc.1 $PREFIX/share/man/man1
 
 mkdir -p $PREFIX/share/yajhfc
 install -m 0644 yajhfc.jar $PREFIX/share/yajhfc
+install -m 0644 logo-large.png $PREFIX/share/yajhfc
+ln -s ../java $PREFIX/share/yajhfc/lib 
 
 mkdir -p $PREFIX/share/pixmaps
 install -m 0644 yajhfc-48x48.png $PREFIX/share/pixmaps/yajhfc.png
 install -m 0644 yajhfc.xpm $PREFIX/share/pixmaps/yajhfc.xpm
+
+mkdir -p $PREFIX/share/icons/hicolor/128x128/apps
+install -m 0644 logo-large.png $PREFIX/share/icons/hicolor/128x128/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/16x16/apps
+install -m 0644 yajhfc-16x16.png $PREFIX/share/icons/hicolor/16x16/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/32x32/apps
+install -m 0644 yajhfc-32x32.png $PREFIX/share/icons/hicolor/32x32/apps/yajhfc.png
+
+mkdir -p $PREFIX/share/icons/hicolor/48x48/apps
+install -m 0644 yajhfc-48x48.png $PREFIX/share/icons/hicolor/48x48/apps/yajhfc.png
 
 mkdir -p $PREFIX/share/applications
 install -m 0644 yajhfc.desktop $PREFIX/share/applications
